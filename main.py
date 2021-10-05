1ae3f283e0e2c85d4455b093dbe57f1dc473f55e
krasav4ik <zkonst12@gmail.com>
2021-10-05 13:07:27 +0300

    Тесты

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random as rand
 import sys
-
+import unittest
 
 all_accords = [a for a in 'ABCDEFG']
 music = []
@@ -8,14 +8,26 @@ music = []
 
 #                   Класс MENU предназначен для взаимодействия с клиентом и
 #                        инициализации списка аккордов и длины мелодии
-class Menu:
+class Menu(unittest.TestCase):
+    accord = None
     accords = None
     length = None
 
     def __init__(self):
+        super().__init__()
         self.accords = []
+        self.accord = []
 
     #               Метод для приветствия и выбора аккордов и длины мелодии
+
+    def insert_accords(self, accord):
+        for a in accord:
+            self.assertIn(a, all_accords, "Введенный аккорд не существует") #Тест на наличие аккордов
+            self.accords.append(a)
+
+    def get_length(self):
+        self.length = int(input())
+        return self.length
 
     def show_menu(self):
         print("Добро пожаловать в прогу по генерации аккордов\n"
@@ -23,13 +35,12 @@ class Menu:
               all_accords,
               "\nВведите аккорды, с которыми хотите сгенерировать мелодию:\n")
         for a in input():
-            if a in all_accords:
-                self.accords.append(a)
-            else:
-                print("Таких аккордов не существует!")
-                sys.exit()
+            self.accord.append(a)
+        self.assertFalse(len(self.accord) == 0, self.accord)    #Тест на нулевой список
+        print(self.accord)
+        self.insert_accords(self.accord)
         print("Выберите длину мелодии: \n")
-        self.length = int(input())
+        self.get_length()
         print("Выбраннее аккорды:", self.accords, "\nДлина мелодии: ", self.length)
 
 
@@ -38,6 +49,7 @@ class Menu:
 class MusicActions():
     def print_music(self):
         print("Полученная мелодия: \n", ''.join(map(str, music)))
+
 
 #                      В данном классе используются аккорды и длина
 #                 заданные в экземпляре класса Menu. Расширяет класс MusicActions
@@ -51,6 +63,24 @@ class Generator(MusicActions):
     def generate_music(self):
         for a in range(self.length):
             music.append(rand.sample(self.accords, 1))
+        return music
+
+#Тесты
+
+class Test(unittest.TestCase):
+    def test_not_empty(self):
+        menu = Menu()
+        menu.accord = ['f', 'a', 'shit']
+        self.assertFalse(len(menu.accord) == 0, menu.accord)
+    def test_equal(self):
+        menu = Menu()
+        menu.accord = ['F', 'A', 'G']
+        for a in menu.accord:
+            self.assertIn(a, all_accords, "Введенный аккорд не существует")
+    def test_for_str(self):
+        tests = ['A', 'F', 'G']
+        test = Generator(5, tests)
+        self.assertFalse(test.generate_music() is list)
 
 
 def main():
@@ -61,5 +91,6 @@ def main():
     generate.print_music()
 
 
+#
 if __name__ == '__main__':
-    main()
+    unittest.main()     #Чтобы запустить программу без тестов: убрать unittest.
